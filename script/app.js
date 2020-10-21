88b05e6bd2aaa1af1b60165f11e1c83569962fdb
Zaac <zaacagnes9@gmail.com>
2020-10-21 16:48:18 -0700

    Added responsive menu to apear/dissapear on click
    Also added animation to menu in javascript

--- script/app.js
+++ script/app.js
@@ -1,3 +1,29 @@
-let navSlide = () => {
-	let burger = document.querySelector('.burger')
+// alert();
+// from: https://www.youtube.com/watch?v=gXkqy0b4M5g
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	//toggle nav
+	burger.addEventListener('click', function(){
+		nav.classList.toggle('nav-active');
+
+		//animate links
+		navLinks.forEach(function(link, index) {
+			// console.log(index);
+			if (link.style.animation) {
+				link.style.animation = ''
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`;
+			}
+			console.log (index / 7);
+		});
+
+		// menu-btn.textContent = "X"
+	});
 }
+
+
+
+navSlide();
---
+++ script/menu.js
@@ -0,0 +1,44 @@
+alert();
+function change() {
+	var menu-button = document.querySelector('.menu-btn');
+
+	if (menu-button.value === "Menu" {
+		// menu-button.textContent = "Close";
+		menu-button.value = "Close";
+		menu-button.innerHTML = "Close";
+	} else {
+		// menu-button.textContent = "Menu";
+		menu-button.value = "Menu";
+		menu-button.innerHTML = "Menu";
+	}
+}
+
+change();
+
+
+
+// function change() {
+// 	let menu-change = document.querySelector('.menu-btn');
+
+// 	if (this.textContent=="Menu") {
+// 		this.texContent="Close";
+// 	} else {
+// 		this.textContent="Menu"
+// 	}
+// }
+
+
+// menu-change.addEventListener("click", function(){
+// 	if (this.textContent==="Menu") {
+// 		this.texContent="Close";
+// 	} else {
+// 		this.textContent="Menu"
+// 	}
+// });
+
+
+
+
+
+
+// modeButtons[i].addEventListener("click", function(){
